0bd95abcf201d6f4c1472e9bcf5eef3dbde87962
sohaila <sohila.bogdady2012@gmail.com>
2018-07-13 21:33:11 +0200

    aggregation for price and brand in matching products

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -8,10 +8,24 @@ exports.search_results = function(req, res) {
 	console.log(query);
 	products.find(query, 'name brand category price').exec(function (err, search_results) {
       if (err) { return next(err); }
-      console.log(search_results);
-      // res.render('searchResults', { title: 'products Result', search_results: search_results });
-  		res.json({"result": search_results})
+      //console.log(search_results);
+      aggQuery = {'$match':{}}
+      aggQuery['$match'][filter] = term
+      
+      if (filter == 'brand'){
+        console.log("brand filter, aggregation group stage by price");
+      	_group = {'$group':{'_id':'$price', 'count':{'$sum':1}} }
+      }
+      else if (filter == 'price') {
+      	console.log("price filter, aggregation group stage by brand");
+      	_group = {'$group':{'_id':'$brand', 'count':{'$sum':1}} }
+      }
+
+      products.aggregate([aggQuery, _group]).exec(function(err, aggregated_result){
+      if (err){return next(err);}
+      res.json({"matching products": search_results, 'aggregation': aggregated_result})
+
+      });
     });
-    //products.aggregate([{ '$match': query }, { '$group': {'_id': 'brand' , 'count': { '$count' : '$brand' } }]);
 
 };
